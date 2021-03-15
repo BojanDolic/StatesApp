da0df92a08d97c907fa2a8cfac3a8ac45d8fd51a
Bojan Dolic <63774884+BojanDolic@users.noreply.github.com>
2021-03-15 01:41:28 +0100

    Created entity

---
+++ app/src/main/java/com/dolic/statesapp/entities/Drzava.java
@@ -0,0 +1,18 @@
+package com.dolic.statesapp.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "drzave")
+class Drzava {
+
+    @PrimaryKey
+    public int d_id;
+
+    @ColumnInfo(name = "Drzave")
+    public String imeDrzave;
+
+    @ColumnInfo(name = "Number")
+    public String brojTelefona;
+}
